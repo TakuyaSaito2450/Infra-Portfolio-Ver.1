d580894b3e1a7eb251d84155a52e3983f604b250
TakuyaSaito2450 <73280375+TakuyaSaito2450@users.noreply.github.com>
2025-06-23 17:47:32 +0900

    Update appserver.tf

--- appserver.tf
+++ appserver.tf
@@ -1,10 +1,31 @@
 # ---------------------------------------------
 # EC2 Instance
 # ---------------------------------------------
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server_1" {
   ami           = "ami-0c3fd0f5d33134a76"
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_1.id
+  #  associate_public_ip_address = true
+  #  key_name = aws_key_pair.tastylog_key.key_name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              amazon-linux-extras install nginx1 -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+}
+
+#resource "aws_key_pair" "tastylog_key" {
+#  key_name   = "tastylog-dev-keypair"
+#  public_key = file("./keys/tastylog-dev-keypair.pub")
+#}
+
+resource "aws_instance" "app_server_2" {
+  ami           = "ami-0c3fd0f5d33134a76"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_2.id
   #  associate_public_ip_address = true
   #  key_name = aws_key_pair.tastylog_key.key_name
 
