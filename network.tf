34af14640cd13e34eb18b380e6bb6019a0e4ef03
TakuyaSaito2450 <73280375+TakuyaSaito2450@users.noreply.github.com>
2025-06-23 17:08:10 +0900

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,45 +1,106 @@
-#####
-#VPC#
-#####
+# ---------------------------------------------
+# VPC
+# ---------------------------------------------
 resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/27"
-  instance_tenancy     = "default"
-  enable_dns_hostnames = true
-  enable_dns_support =  true
+  cidr_block = "10.0.0.0/26"
+
+  tags = {
+    Name = "my-vpc"
+  }
 }
 
-########
-#Subnet#
-########
-resource "aws_subnet" "public_subnet_1a" {
+# ---------------------------------------------
+# Subnet
+# ---------------------------------------------
+resource "aws_subnet" "public_1" {
   vpc_id            = aws_vpc.vpc.id
   availability_zone = "ap-northeast-1a"
-  map_public_ip_on_launch = true
-  cidr_block        = "10.0.0.0/27"
+  cidr_block        = "10.0.0.0/28"
+
+  tags = {
+    Name = "my-subnet-public_1"
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.0.16/28"
+
+  tags = {
+    Name = "my-subnet-public_2"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.0.32/28"
+
+  tags = {
+    Name = "my-subnet-private_1"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.vpc.id
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.0.48/28"
+
+  tags = {
+    Name = "my-subnet-private_2"
+  }
+}
+
+# ---------------------------------------------
+# Route Table
+# ---------------------------------------------
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_1" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_1.id
+}
+
+resource "aws_route_table_association" "public_2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public_2.id
 
 }
 
-########
-#Route Table#
-########
-resource "aws_route_table" "public_rt" {
+resource "aws_route_table" "private" {
   vpc_id = aws_vpc.vpc.id
 }
 
-resource "aws_route_table_association" "public_rt_1a" {
-  route_table_id = aws_route_table.public_rt.id
-  subnet_id      = aws_subnet.public_subnet_1a.id
+resource "aws_route_table_association" "private_1" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_1.id
 }
 
-########
-#Internet Gateway#
-########
+resource "aws_route_table_association" "private_2" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private_2.id
+}
+
+# ---------------------------------------------
+# Internet Gateway
+# ---------------------------------------------
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "my-igw"
+  }
 }
 
-resource "aws_route" "public_rt_igw_r" {
-  route_table_id         = aws_route_table.public_rt.id
-  destination_cidr_block = "0.0.0.0/0"
+resource "aws_route" "public_igw" {
+  route_table_id         = aws_route_table.public.id
   gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
 }
